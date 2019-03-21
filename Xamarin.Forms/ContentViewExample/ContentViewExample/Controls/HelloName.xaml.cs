af84fa17110148636890eeaeec41e55576a03030
brzooz <jwierzbanowski@gmail.com>
2019-03-21 16:27:47 +0100

    Init commit. Added example for first post about vue and Xamarin.Forms.

---
+++ Xamarin.Forms/ContentViewExample/ContentViewExample/Controls/HelloName.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ContentViewExample.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HelloName : ContentView
+    {
+        public static readonly BindableProperty NameProperty =
+     BindableProperty.Create(nameof(Name), typeof(string), typeof(HelloName));
+
+        public HelloName()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        public string Name
+        {
+            get { return (string)GetValue(NameProperty); }
+            set { SetValue(NameProperty, value); }
+        }
+    }
+}
