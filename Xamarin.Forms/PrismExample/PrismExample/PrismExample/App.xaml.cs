2911df773b9af5f1c02be1a486db221f8031b2cb
brzooz <jwierzbanowski@gmail.com>
2019-03-24 21:04:48 +0100

    Added first example for Prism.

---
+++ Xamarin.Forms/PrismExample/PrismExample/PrismExample/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Prism.Ioc;
+using Prism.Unity;
+using PrismExample.Views;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+namespace PrismExample
+{
+    public partial class App : PrismApplication
+    {
+        protected async override void OnInitialized()
+        {
+            InitializeComponent();
+            await NavigationService.NavigateAsync(nameof(HomeView));
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<HomeView>();
+        }
+    }
+}
---
+++ Xamarin.Forms/PrismExample/PrismExample/PrismExample/ViewModels/HomeViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System.Windows.Input;
+
+namespace PrismExample.ViewModels
+{
+    public class HomeViewModel : BindableBase
+    {
+        private int _count;
+
+        public int Count
+        {
+            get { return _count; }
+            set
+            {
+                _count = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public ICommand IncrementCountCommand => new DelegateCommand(IncrementCount);
+
+        private void IncrementCount()
+        {
+            Count++;
+        }
+    }
+}
