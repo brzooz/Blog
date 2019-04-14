68efa453c65f37a1bd2887c570a0b640df006e96
brzooz <jwierzbanowski@gmail.com>
2019-04-14 16:01:02 +0200

    Added example for ActionResult.

---
+++ c#/ActionResult/ActionResult/Controllers/ValuesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ActionResult.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("1")]
+        public IEnumerable<string> GetValuesEnumerable()
+        {
+            return new[]
+            {
+                "hello", "world"
+            };
+        }
+
+        [HttpGet("2")]
+        public IActionResult GetValuesIActionResult()
+        {
+            var values = new[]
+            {
+                "hello", "world"
+            };
+
+            return Ok(values);
+        }
+        
+        [HttpGet("3")]
+        public ActionResult<IEnumerable<string>> GetValuesActionResult()
+        {
+            return new[]
+            {
+                "hello", "world"
+            };
+        }
+    }
+}
