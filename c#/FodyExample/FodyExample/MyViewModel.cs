41aa6e0168f8e1dcfc9fe0d2df812958439b7c9a
brzooz <jwierzbanowski@gmail.com>
2019-05-06 22:13:14 +0200

    Init commit for FodyExample.

---
+++ c#/FodyExample/FodyExample/MyViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace FodyExample
+{
+    public class MyViewModel : INotifyPropertyChanged
+    {
+        public MyViewModel()
+        {
+            PropertyChanged += MyViewModel_PropertyChanged;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int Count { get; set; }
+        public string Text { get; set; }
+
+        private void MyViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            Console.WriteLine($"Property changed - {e.PropertyName}");
+        }
+    }
+}
---
+++ c#/FodyExample/FodyExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FodyExample
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var viewModel = new MyViewModel
+            {
+                Text = "text",
+                Count = 1
+            };
+        }
+    }
+}
